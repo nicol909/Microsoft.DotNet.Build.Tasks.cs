5ae1339c326c6edd238fc723b990753ec0c41d10
nicoleputois <nicoleputois@hotmail.com>
2019-12-10 21:09:20 +0100

    Ajoutez des fichiers projet.

---
+++ Microsoft.DotNet.Build.Tasks.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microsoft.DotNet.Build.Tasks.cs
+{
+    class Program
+        
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+    }
+}
